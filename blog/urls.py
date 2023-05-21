067c14e1c582bfd97b99aeb24caa1c513f0121b5
hugotata78 <hugotatarinoff@gmail.com>
2023-05-20 22:47:08 -0300

    prueba conectando admin

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('posts.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),

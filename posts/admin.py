0c34522ac5822bb05bc780e4ebe95f76f26f306c
hugotata78 <hugotatarinoff@gmail.com>
2023-05-27 18:32:52 -0300

    se agregó variable de entorno FRONT_SITE_URL

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Post, Comment, Category
+import os
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Category)
+
+admin.site.site_url = os.environ.get('FRONT_SITE_URL', default='http://127.0.0.1:5173')

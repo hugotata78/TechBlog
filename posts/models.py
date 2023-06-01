6a359303b7eb316ff942377b1938e2271b224047
hugotata78 <carlitosyagus_@hotmail.com.ar>
2023-06-01 15:46:36 -0300

    modificación modelos category y post

--- posts/models.py
+++ posts/models.py
@@ -2,11 +2,25 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    owner = models.ForeignKey(get_user_model(), related_name='categories',on_delete=models.CASCADE)
+    
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(get_user_model(), related_name='posts', on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category,related_name='posts', blank=True)
 
     class Meta:
         ordering = ['-created']
@@ -26,14 +40,3 @@ class Comment(models.Model):
     def __str__(self):
         return self.comment
 
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(blank=True, null=True)
-    owner = models.ForeignKey(get_user_model(), related_name='categories',on_delete=models.CASCADE)
-    posts = models.ManyToManyField(Post,related_name='categories', blank=True)
-
-    class Meta:
-        ordering = ['name']
-
-    def __str__(self):
-        return self.name

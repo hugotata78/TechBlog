1ab126cfdb025052e924059459d84470561d23dc
hugotata78 <carlitosyagus_@hotmail.com.ar>
2023-06-01 18:11:05 -0300

    cambios en configuración admin user

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from . models import User
+import os
+
+#configuraciones adminUser
+
+class adminUser(admin.ModelAdmin):
+    list_display =('username', 'email','first_name','last_name')
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, adminUser)
+
+#admin.site.site_url = os.environ.get('FRONT_SITE_URL', default='http://127.0.0.1:5173')

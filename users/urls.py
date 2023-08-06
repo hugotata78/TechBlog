1e9296192d6a517fd29990cd42326e9be19f2e8a
hugotata78 <carlitosyagus_@hotmail.com.ar>
2023-08-06 17:39:58 -0300

    se quitó comentarios innecesarios

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import UserView, MyTokenObtainPairView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenBlacklistView
 
 router = routers.DefaultRouter()
 router.register('', UserView, 'users')
@@ -9,11 +9,7 @@ router.register('', UserView, 'users')
 
 urlpatterns = [
     path('',include(router.urls) ),
-    #Endpoints de usuarios: Metodos Post y Get no requieren autenticación
-    #Metodos Put, Patch y Delete requieren autenticación y ser propietario del perfil para realizar estas acciones
     path('auth/token/', MyTokenObtainPairView.as_view(), name='token'),
     path('auth/token_refresh/', TokenRefreshView.as_view(), name='refresh'),
     path('auth/logout/', TokenBlacklistView.as_view(), name='logout'),
-    #Login de usuario genera un token de autenticación
-    #El usuario debe estar previamente registrado y deberá ingresar su usuario y contraseña para autenticarse
 ]
